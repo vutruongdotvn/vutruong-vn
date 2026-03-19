c19b5da5eddb2c99cbf383a68ab799a388241170
Vũ Trường <admin@vutruong.vn>
2026-03-19 16:19:19 +0700

    update 20260319161919

--- app/bio/page.tsx
+++ app/bio/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import type { Metadata } from "next";
-import { Metadata } from 'next'
+import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Bio',
