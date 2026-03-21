0a2d4d2041f53164fc8d80f1ce7a15fc5ef6f78c
Vũ Trường <admin@vutruong.vn>
2026-03-21 16:43:25 +0700

    update 20260321164325

--- lib/posts.ts
+++ lib/posts.ts
@@ -2,21 +2,21 @@ export const posts = [
   {
     id: "1",
     author: "Vũ Trường",
-    time: "5 phút trước",
+    time: "5 phút",
     content: "Đây là bài viết demo đầu tiên 🚀",
     images: ["/og.png"],
   },
   {
     id: "2",
     author: "Vũ Trường",
-    time: "10 phút trước",
+    time: "3 giờ",
     content: "Layout 3 ảnh đẹp hơn",
     images: ["/og.png", "/og.png", "/og.png"],
   },
   {
     id: "3",
     author: "Vũ Trường",
-    time: "30 phút trước",
+    time: "Hôm qua",
     content: "Nhiều ảnh test UI 😎",
     images: [
       "/og.png",
