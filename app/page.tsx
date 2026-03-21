ef3ff276941f2e3bb74b212c85b7de5966e49891
Vũ Trường <admin@vutruong.vn>
2026-03-21 11:28:46 +0700

    update 20260321112846

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Page() {
 
           {/* Subtitle */}
           <p className="text-gray-600 text-base">
-            Website đang build<br/>nên không có gì để hiển thị ở đây cả 🧐
+            Website đang build...<br/>không có gì ở đây cả 🧐
           </p>
 
           <Countdown />
