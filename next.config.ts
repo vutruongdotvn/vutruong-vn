1f069eaf8d6a340648769191edf854414db1f127
Vũ Trường <admin@vutruong.vn>
2026-03-25 17:29:08 +0700

    update 20260325172907

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
+const nextConfig: NextConfig = {
   images: {
-    domains: ["frqouslfjppnpcpgrhjd.supabase.co", "ui-avatars.com", "res.cloudinary.com"],
+    domains: [
+      "frqouslfjppnpcpgrhjd.supabase.co",
+      "ui-avatars.com",
+      "res.cloudinary.com",
+    ],
+
+    // Ảnh responsive lớn: post image, cover, card...
+    deviceSizes: [640, 750, 828, 1080],
+
+    // Ảnh nhỏ: avatar, thumbnail, icon...
+    imageSizes: [32, 48, 64, 96, 128, 256, 384],
   },
 };
 
