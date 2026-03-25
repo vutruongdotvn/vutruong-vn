7cf1defbda307bb8e6090aef530675cab23014bc
Vũ Trường <admin@vutruong.vn>
2026-03-25 17:30:30 +0700

    update 20260325173030

--- lib/compressImage.ts
+++ lib/compressImage.ts
@@ -2,9 +2,10 @@ import imageCompression from "browser-image-compression";
 
 export async function compressImage(file: File) {
   const options = {
-    maxSizeMB: 1, // giữ chất lượng cao
-    maxWidthOrHeight: 1920, // không resize quá nhỏ
+    maxSizeMB: 1.2,
+    maxWidthOrHeight: 1920,
     useWebWorker: true,
+    initialQuality: 0.9,
   };
 
   return await imageCompression(file, options);
