7d3cdafa30c9710ec01723b520c9a9570f81967d
Vũ Trường <admin@vutruong.vn>
2026-03-26 08:03:43 +0700

    update 20260326080343

--- components/blog/PostActions.tsx
+++ components/blog/PostActions.tsx
@@ -3,13 +3,14 @@
 export default function PostActions() {
   return (
     <div className="postAction flex items-center gap-3 p-3">
-      <button className="cursor-pointer text-gray-600 hover:text-black" title="Thích">
+      {/* <button className="cursor-pointer text-gray-600 hover:text-black" title="Thích">
         <i className="fa-duotone fa-heart" />
       </button>
 
       <button className="cursor-pointer text-gray-600 hover:text-black" title="Bình luận">
         <i className="fa-duotone fa-comment" />
       </button>
+      */}
 
       <button className="cursor-pointer text-gray-600 hover:text-black" title="Chia sẻ">
         <i className="fa-duotone fa-share" />
