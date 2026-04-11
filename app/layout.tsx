62d304d9871b0d06d7e81faaa615b19f4f86b163
Vũ Trường <admin@vutruong.vn>
2026-04-11 11:44:35 +0700

    update 20260411114435

--- app/layout.tsx
+++ app/layout.tsx
@@ -93,7 +93,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="vi" className={roboto.variable}>
+    <html lang="vi" id="vt-zone" className={`${roboto.variable} scrollbar-hide`}>
       <head>
         <link rel="manifest" href="/manifest.json" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
