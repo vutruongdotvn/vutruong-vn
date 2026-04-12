030052df17848f1d018833a599fd9f57b8aefcfd
Vũ Trường <admin@vutruong.vn>
2026-04-12 22:41:02 +0700

    fix: remove unused cloudinaryLoader import

--- components/blog/PostImages.tsx
+++ components/blog/PostImages.tsx
@@ -3,7 +3,6 @@
 import Image from "next/image";
 import { useEffect, useMemo, useRef, useState } from "react";
 import {
-  cloudinaryLoader,
   extractCloudinaryMeta,
   getFeedImage,
   getLightboxImage,
